bb056189639e3ee93170294e3d0ac3b4eb3d3454
ricardoblancobarbara <ricardoblancobarbara@gmail.com>
2019-12-03 15:06:55 -0500

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,70 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { FormsModule } from '@angular/forms';
+//import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+//import { RegisterComponent } from './register/register.component';
+//import { LoginComponent } from './login/login.component';
+//import { HomeComponent } from './home/home.component';
+
+
+//export class AppModule { }
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// used to create fake backend
+//import { fakeBackendProvider } from './_helpers';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        FormsModule,
+        RouterModule,
+        NoopAnimationsModule,      
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AlertComponent,
+        AppComponent,
+        SignupComponent,
+        FooterComponent,
+        HeaderComponent,
+        RegisterComponent,
+        LoginComponent,
+        HomeComponent,
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        //fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
--- src/app/fetch.service.ts
+++ src/app/fetch.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { IUsers } from '../app/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchService {
 
-  constructor(private httpClient: HttpClient) { }
+  private _url: string = "http://localhost:3000/user/login";
 
-  public getUser() {
-    return this.httpClient.get(`http://user/create`)
+  constructor(private http: HttpClient) { }
+
+  public getUser(): Observable<IUsers[]>{
+    return this.http.get<IUsers[]>(this._url);
   }
 
 }
