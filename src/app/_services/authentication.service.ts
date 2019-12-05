1d9336fd78fe6a236c15352021999dfbb1a7e326
ricardoblancobarbara <ricardoblancobarbara@gmail.com>
2019-12-05 13:02:39 -0500

    reg

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -28,6 +28,7 @@ export class AuthenticationService {
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
+                    console.log(localStorage);
                     this.currentUserSubject.next(user);
                 }
                 console.log(username, password, user);
