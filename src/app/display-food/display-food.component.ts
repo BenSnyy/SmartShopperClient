78ad00953802cd12b6e5e53539e9873ee1ea3e48
ricardoblancobarbara <ricardoblancobarbara@gmail.com>
2019-12-14 09:25:14 -0500

    update method

--- src/app/display-food/display-food.component.ts
+++ src/app/display-food/display-food.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, from } from 'rxjs';
 import { first } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { Food } from '../_models/food.model';
 import { AuthenticationService } from '../_services';
 import { DatabaseService } from '../_services/database.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UpdateFoodComponent } from '../update-food/update-food.component';
+import { ModalContentComponent } from '../modal-content/modal-content.component'
 
 @Component({
   selector: 'app-display-food',
@@ -17,8 +18,24 @@ import { UpdateFoodComponent } from '../update-food/update-food.component';
 export class DisplayFoodComponent implements OnInit { //this will store the food in _food
   //currentUser: User;
   currentUserSubscription: Subscription;  
-  public _food = {};
+  public _food = {
+    id: 10,
+    food: 'Cookies',
+    quantity: 26,
+    expiration: 2020/12/24,
+    brand: 'RB',
+  }
+
+  // this.updateFood = this.fb.group({
+  //   food: '',
+  //   quantity: '',
+  //   expiration: '',
+  //   brand: ''
+  // })
+
   foods: Food[] = [];
+  @Input() id: number;
+  
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -37,21 +54,45 @@ export class DisplayFoodComponent implements OnInit { //this will store the food
       });
   }
 
+
+
+
   // getFoods() {
   //   this.dbService.getFood().pipe<Food[]>(first()).subscribe(foods => {
   //       this.foods = foods;
   //   });
   // }
 
-  openFormModal() {
-    const modalRef = this.modalService.open(UpdateFoodComponent);
-    
+  // openUpdateFood(id: number) {
+  //   const modalRef = this.modalService.open(UpdateFoodComponent);
+  //   modalRef.componentInstance._food.id = 10;//this._food.id // should be the id
+
+  //   modalRef.result.then((result) => {
+  //     console.log(result);
+  //   }).catch((error) => {
+  //     console.log(error);
+  //   });
+  // }
+
+  openModal() {
+    const modalRef = this.modalService.open(ModalContentComponent);
+    modalRef.componentInstance._food = this._food;
+    //console.log(this._food.id); //this has all the info updated required
     modalRef.result.then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      console.log(error);
+      if (result) {
+        console.log(result);
+        //UpdateFood()
+        //this._food(result)
+        this.dbService.updateFood(this._food.id)
+          .subscribe(result => {this.foods = result }); //should be this._food
+          console.log(result);    
+      }
     });
+    // modalRef.componentInstance.passEntry.subscribe((receivedEntry) => {
+    //   console.log(receivedEntry);
+    // })
   }
+
 
   // updateFood(id:number) {
   //   this.dbService.updateFood(id).pipe(first()).subscribe(() => {
--- src/app/update-food/update-food.component.ts
+++ src/app/update-food/update-food.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { first } from 'rxjs/operators';
 
+import { DatabaseService } from '../_services/database.service';
 
 @Component({
   selector: 'app-update-food',
@@ -8,35 +11,57 @@ import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bo
   styleUrls: ['./update-food.component.css']
 })
 export class UpdateFoodComponent implements OnInit {
-  closeResult: string;
   
+  closeResult: string;  
+  @Input() public _food;
+  @Output() passEntry: EventEmitter<any> = new EventEmitter;
+  myForm: FormGroup;
+  
+  constructor(
+    private dbService: DatabaseService,
+    private modalService: NgbModal, 
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder
+    ) {
+      this.createForm();
+    }
+    
+    ngOnInit() {
+      console.log(this._food);
+    }
+    
+    
+    
 
-  constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) { }
+
+  updateFood(id: number) {
+    this.dbService.updateFood(id).pipe(first()).subscribe(() => {
+    
+      //this.getFoods();
+    });
+  }
+
+  private createForm() {
+    this.myForm = this.formBuilder.group({
+      id: '',
+      food: '',
+      quantity: '',
+      expiration: '',
+      brand: '',
+    });
+  }
+
+  private submitForm() {
+    this.activeModal.close(this.myForm.value);
+  }
 
   closeModal() {
     this.activeModal.close('Modal Closed');
   }
 
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
-
-
-  ngOnInit() {
+  passBack() {
+    this.passEntry.emit(this._food);
+    this.activeModal.close(this._food);
   }
 
 }
