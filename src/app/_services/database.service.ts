4d8080612c2e919824048c99b15ae94204ce6d89
Ben Snyder <bengsnyder@gmail.com>
2019-12-16 14:18:01 -0500

    update

--- src/app/_services/database.service.ts
+++ src/app/_services/database.service.ts
@@ -50,15 +50,15 @@ export class DatabaseService {
   }
 
 //Updates or Patchs  
-  // updateFood(id: number, food: Food) {
-  //   return this.http.put<Food[]>(`${environment.apiUrl}/food/update/${id}`, food, httpOptions)
-  // }
+  updateFood(id: number, food: Food) {
+    return this.http.put<Food[]>(`${environment.apiUrl}/food/update/${id}`, food, httpOptions)
+  }
   
   updateShopping(id: number, shopping: Shopping) {
     return this.http.put<Shopping[]>(`${environment.apiUrl}/shopping/update/${id}`, shopping, httpOptions)
   }  
 
-  updateFood(id: number) {
+  updateFoods(id: number) {
     return this.http.put<Food[]>(`${environment.apiUrl}/food/update/${id}`, id, httpOptions)
   } 
   
